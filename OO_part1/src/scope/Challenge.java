625a3d2c226d913f8b82c2024e257f2f29934737
Michael-Benecaid <mzhang@benecaid.com>
2018-05-24 14:05:58 -0400

    Scope challenge finished

---
+++ OO_part1/src/scope/Challenge.java
@@ -0,0 +1,16 @@
+package scope;
+
+import java.util.Scanner;
+
+public class Challenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Please enter a number");
+//		X x = new X(scanner.nextInt());
+		X x = new X(new Scanner(System.in));
+		x.x();
+	}
+
+}
