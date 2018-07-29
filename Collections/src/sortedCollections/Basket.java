d4c15538d4cefa7326a5e973758ec01607ad66b9
Michael <mzhang@benecaid.com>
2018-07-29 15:31:07 -0400

    to be continue

---
+++ Collections/src/sortedCollections/Basket.java
@@ -0,0 +1,20 @@
+package sortedCollections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Basket {
+	private final String name;
+	private final Map<StockItem, Integer> list;
+	
+	public Basket(String name) {
+		this.name = name;
+		this.list = new HashMap<>();
+	}
+	
+	public int addToBasket(StockItem item, int quantity) {
+		if((item != null) && (quantity > 0)) {
+			int inBasket = list.getOrDefault(item, 0);
+		}
+	}
+}
---
+++ Collections/src/sortedCollections/Main.java
@@ -0,0 +1,10 @@
+package sortedCollections;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
