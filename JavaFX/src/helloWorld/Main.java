29204d027e85cebfac6e1ebd062297a3e04be06c
Michael-Benecaid <mzhang@benecaid.com>
2018-07-30 16:01:39 -0400

    To be continue. Move to next section

--- JavaFX/src/helloWorld/Main.java
+++ JavaFX/src/helloWorld/Main.java
@@ -1,21 +1,38 @@
 package helloWorld;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+//			BorderPane root = new BorderPane();
+			
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+//			GridPane root = new GridPane();
+//			root.setAlignment(Pos.CENTER);
+//			root.setVgap(10);
+//			root.setHgap(10);
+			
+			Parent root = FXMLLoader.load(getClass().getResource("helloWorld.fxml"));
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Hello World");
+			primaryStage.setTitle("Hello JavaFX!");
 			primaryStage.show();
+
+//			Label greeting = new Label("Welcome to JavaFX!");
+//			greeting.setTextFill(Color.GREEN);
+//			greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//			
+//			root.getChildren().add(greeting);
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
