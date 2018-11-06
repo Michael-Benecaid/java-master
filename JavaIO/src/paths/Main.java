3571770fccc42f2f8cbd1ca787f8b4e7e1cf19b1
Michael <mzhang@benecaid.com>
2018-11-05 19:39:39 -0500

    added files

--- JavaIO/src/paths/Main.java
+++ JavaIO/src/paths/Main.java
@@ -1,7 +1,33 @@
 package paths;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+        printFile(path);
+        Path path2 = FileSystems.getDefault().getPath("./files/SubdirectoryFile.txt");
+        printFile(path2);
+        Path filePath = Paths.get("/Users/mzhang/Documents/Project/java-master/OutThere.txt");
+        printFile(filePath);
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
+    }
+
+    private static void printFile(Path path){
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = fileReader.readLine())!= null) {
+                System.out.println(line);
+            }
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
